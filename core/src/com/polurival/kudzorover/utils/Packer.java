a98a911688103d02559fc7a13f3b2ed73a1286b8
Polurival <polurival@gmail.com>
2017-02-26 18:48:04 +0300

    added resources

---
+++ core/src/com/polurival/kudzorover/utils/Packer.java
@@ -0,0 +1,25 @@
+package com.polurival.kudzorover.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+/**
+ * Created by Polurival
+ * on 26.02.2017.
+ */
+
+public class Packer {
+    // TODO: 26.02.2017 run this for creating texture files
+    public static void main(String[] args) {
+        TexturePacker.Settings set = new TexturePacker.Settings();
+        set.filterMin = Texture.TextureFilter.MipMapLinearNearest;
+        set.filterMag = Texture.TextureFilter.Linear;
+        set.paddingX = 2;
+        set.paddingY = 2;
+        set.maxHeight = 2048;
+        set.maxWidth = 2048;
+
+        TexturePacker.process(set, "raw_images_ru", "android/assets/images_ru", "pack");
+        TexturePacker.process(set, "raw_images_en", "android/assets/images_en", "pack");
+    }
+}
