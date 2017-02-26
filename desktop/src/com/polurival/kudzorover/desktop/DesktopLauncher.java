4f3be2368c0d12b70dbdbf52b05332e44e2b3708
Polurival <polurival@gmail.com>
2017-02-26 18:02:17 +0300

    Initial commit

---
+++ desktop/src/com/polurival/kudzorover/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.polurival.kudzorover.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.polurival.kudzorover.LunarRover;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LunarRover(), config);
+	}
+}
